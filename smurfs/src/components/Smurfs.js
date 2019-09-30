0dcaf4ad996959f9d9ba60ce167c0187ad5f48d9
Cody Caro <codycaro@Codys-MacBook-Pro.local>
2019-09-29 19:56:13 -0700

    Added some styling to the smurfs

--- smurfs/src/components/Smurfs.js
+++ smurfs/src/components/Smurfs.js
@@ -1,8 +1,44 @@
 import React, { useEffect } from "react";
 import "./App.css";
-import { getSmurf, addSmurf } from "../actions";
+import styled from "styled-components";
 
+import { getSmurf } from "../actions";
 import { connect } from "react-redux";
+import smurfImage from "../smurf.png";
+
+const SmurfHolder = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
+const SmurfCard = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  border: solid 1px dodgerblue;
+  margin: 10px 8%;
+  width: 10%;
+  img {
+    width: 100%;
+    border-bottom: solid 2px dodgerblue;
+    background: dodgerblue;
+  }
+`;
+const DetailHolder = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+`;
+const SmurfName = styled.h2`
+  margin: 10px 0;
+  padding-bottom: 10px;
+  border-bottom: solid 1px dodgerblue;
+  width: 100%;
+`;
+const SmurfDetail = styled.h3`
+  margin: 5px auto;
+`;
 
 const Smurfs = ({ getSmurf, smurfs, isFetching, error }) => {
   useEffect(() => {
@@ -14,17 +50,20 @@ const Smurfs = ({ getSmurf, smurfs, isFetching, error }) => {
   }
 
   return (
-    <div>
+    <SmurfHolder>
       {smurfs.map(smurf => {
         return (
-          <div>
-            <h2>{smurf.name}</h2>
-            <h3>{smurf.age}</h3>
-            <h3>{smurf.height}</h3>
-          </div>
+          <SmurfCard>
+            <img src={smurfImage} />
+            <DetailHolder>
+              <SmurfName>{smurf.name}</SmurfName>
+              <SmurfDetail>Age: {smurf.age}</SmurfDetail>
+              <SmurfDetail>Height: {smurf.height}</SmurfDetail>
+            </DetailHolder>
+          </SmurfCard>
         );
       })}
-    </div>
+    </SmurfHolder>
   );
 };
 
