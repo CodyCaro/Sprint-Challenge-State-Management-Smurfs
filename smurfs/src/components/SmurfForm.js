a44bf969561e39ba513718b251c0b03a2f647cae
Cody Caro <codycaro@Codys-MacBook-Pro.local>
2019-09-29 19:11:27 -0700

    Form will submit a new smurf

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,21 +1,54 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { getSmurf, addSmurf } from "../actions";
+import useForm from "react-hook-form";
 
 const SmurfForm = ({ getSmurf, addSmurf, smurfs, isFetching, error }) => {
   useEffect(() => {
     // run action creator when the component mounts
     getSmurf();
-    addSmurf({ name: "Working", age: 5, height: "5cm" });
   }, [getSmurf]);
 
-  console.log(smurfs);
+  const { handleSubmit, register, errors } = useForm();
+  const onSubmit = values => {
+    console.log(values);
+    addSmurf(values);
+  };
 
   if (isFetching) {
     return <h3>Fetching smurfs!</h3>;
   }
 
-  return <div></div>;
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          name="name"
+          ref={register({
+            required: "Required"
+          })}
+        />
+        {errors.name && errors.name.message}
+
+        <input
+          name="age"
+          ref={register({
+            required: "Required"
+          })}
+        />
+        {errors.age && errors.age.message}
+        <input
+          name="height"
+          ref={register({
+            required: "Required"
+          })}
+        />
+        {errors.height && errors.height.message}
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 const mapStateToProps = state => {
